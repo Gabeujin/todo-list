4bac4068c3d871c74a2614cf92255748b7fd12f6
Gabeujin <31458110+Gabeujin@users.noreply.github.com>
2023-11-16 14:16:25 +0000

    Update 4 files

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
+const port = 3000;
 const express = require("express");
-const usersRouter = require("./routes/users");
 const usersTodos = require("./routes/todo");
 const app = express();
-const port = 3000;
-
 //view engine 사용
 app.set("view engine", "ejs");
 app.set("views", "./views");
+//라우터 지정
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use("/todo", usersTodos);
 
-app.get("/", (req, res) => {
+app.get("/", (req, res, next) => {
   //홈페이지 지정
-  //res.sendFile(__dirname + viewPath + "/index.html");
-  var vsSubTitle = "express js";
+  var vsSubTitle = "nodeJs x express web framework(CodeSandbox)";
   //view engine 사용시
   res.render("index", {
-    title: `node.js with ${vsSubTitle} and viewEngine EJS`,
+    title: `${vsSubTitle}`,
   });
 });
-
-//라우터 지정
-app.use("/users", usersRouter);
-//app.use("/todo", usersTodos);
 
 app.listen(port, () => {
   //port listening 중일때 아래 코듯 실행
--- routes/todo.js
+++ routes/todo.js
@@ -1,15 +1,19 @@
 var express = require("express"); //웹 프레임워크 express
 var router = express.Router(); //라우터 사용시 필히 명시
 
-/* GET users listing. */
-router.post("/", function (req, res, next) {
-  const title = "todoList";
-  console.log(res);
-  // const vsNm = res.body.nm;
-  // const vsGender = res.body.gender;
-  // res.send(`<h1>${title}</h1>
-  //   <p>당신의 이름은 ${vsNm}</p>
-  //   <p>당신의 성별은 ${vsGender}</p>`);
+/* Post users listing. queryString 에서 바로 가져옴 */
+router.get("/", (req, res, next) => {
+  // let vsNm = req.query.nm;
+  // let vsGender = req.query.gender;
+  res.render("todo", {
+    title: `TODO LIST`,
+  });
+});
+
+/* Post users listing. - used body parser(express v4 ~ builtIn) */
+router.post("/", (req, res, next) => {
+  // let vsNm = req.body.nm;
+  // let vsGender = req.body.gender;
 });
 
 //라우터 모듈을 export로 추출(index.js 의 상단에 선언한 require("./routes/todo"); 사용하기위해 필수 절차)
